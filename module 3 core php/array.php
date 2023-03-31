f0d98f1e84eb3f438f83bc0d48d70aba3cd6e854
Jignapatel01 <118799751+Jignapatel01@users.noreply.github.com>
2023-03-31 16:02:49 +0530

    Add files via upload

---
+++ module 3 core php/array.php
@@ -0,0 +1,17 @@
+<?php
+
+$name=array("a"=>"jigna","b"=>"bharat","c"=>"tanni","d"=>"jeny");
+print_r($name);
+echo "<br>";
+
+$arr=array("brijesh","yash","harnish");
+print_r($arr);
+ echo "<br>";
+
+$name=array("a"=>"10","b"=>"20","c"=>"30","d"=>"40");
+print_r($name);
+echo "<br>";
+
+$name=array("0"=>"1","1"=>"2","2"=>"3","3"=>"4");
+print_r($name);
+?>
